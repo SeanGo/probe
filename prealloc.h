644d1ea8e24441d28bd176e35ed0e4e2d68012cb
SeanGo <stoneguo@126.com>
2015-11-06 13:35:49 +0800

    First Version

---
+++ prealloc.h
@@ -0,0 +1,18 @@
+/*
+ * sysstat: System performance tools for Linux
+ * (C) 1999-2015 by Sebastien Godard (sysstat <at> orange.fr)
+ */
+
+#ifndef _PREALLOC_H
+#define _PREALLOC_H
+
+/* Preallocation constants for sar */
+#define NR_IFACE_PREALLOC	(2 * 1)
+#define NR_SERIAL_PREALLOC	(2 * 1)
+#define NR_DISK_PREALLOC	(3 * 1)
+#define NR_FREQ_PREALLOC	(0 * 1)
+#define NR_USB_PREALLOC		(5 * 1)
+#define NR_FILESYSTEM_PREALLOC	(3 * 1)
+#define NR_FCHOST_PREALLOC	(1 * 1)
+
+#endif  /* _PREALLOC_H */
